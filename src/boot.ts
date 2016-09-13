dfdfeb0704b28450bf940efbfd70b62ad3f58ed8
Richard N. Sparrow <rsparrow@me.com>
2016-09-13 10:52:33 -0700

    Removed old style namespace…

--- src/boot.ts
+++ src/boot.ts
@@ -8,7 +8,5 @@ import "../node_modules/angular-aria/angular-aria.js";
 
 import {MainController} from "./controllers/mainController.ts";
 
-module ProjectTemplateApp {
-    angular.module('projectTemplateApp', ['ngMaterial'])
-        .controller('mainController', MainController);
-}
+angular.module('projectTemplateApp', ['ngMaterial'])
+    .controller('mainController', MainController);
